9f01b3934947d078d747ec1fbcc1c0f87e40afb3
Edona Lleshi <edona.lleshi95@gmail.com>
2024-01-09 16:28:16 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    const holidayValue = event.target.elements.holiday.value;
+    const dateValue = event.target.elements.date.value;
+
+    setHoliday(holidayValue);
+    setDate(dateValue);
+
+    event.target.reset();
+
+    document.getElementById("holiday").focus();
   }
 
   return (
@@ -30,10 +43,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
